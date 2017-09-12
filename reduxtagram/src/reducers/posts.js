63d9216af5f94937532dcafb4e01eac64d1e3909
amunozdv@gmail.com <amunozdv@gmail.com>
2017-09-12 03:03:47 +0000

    Updating State with Reducres - Increment Likes reducer

--- reduxtagram/src/reducers/posts.js
+++ reduxtagram/src/reducers/posts.js
@@ -4,7 +4,18 @@
 // 3 and returns the next state
 
 function posts(state = [], action) {
-  console.log(state, action);
+  const i = action.index;
+  switch(action.type){
+    case 'INCREMENT_LIKES':
+      //return the updated state
+      return [
+        ...state.slice(0, i), //before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1)//after the one we are updating
+      ]
+    default:
+      return state;
+  }
   return state;
 }
 
