db12621b0e876ad97d9c4e353dd2840fec8d6274
amunozdv@gmail.com <amunozdv@gmail.com>
2017-09-21 00:24:36 +0000

    update comment state (add/remove) from our Store

--- reduxtagram/src/components/Comments.js
+++ reduxtagram/src/components/Comments.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 class Comments extends React.Component {
 
-  renderComments(comment, i){
-    return(
+  constructor(){
+    super();
+    this.renderComments = this.renderComments.bind(this);
+  }
+
+  renderComments(comment, i) {
+    return (
       <div className="comment" key={i}>
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button onClick={this.props.removeComment.bind(null, this.props.match.params.postId, i)} className="remove-comment">&times;</button>
         </p>
       </div>
     )
@@ -20,17 +25,18 @@ class Comments extends React.Component {
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
     this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   }
 
   render() {
     return <div className="comments">
-        {this.props.postComments.map(this.renderComments)}
-                <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit.bind(this)}>
-                  <input type="text" ref="author" placeholder="author"/>
-                  <input type="text" ref="comment" placeholder="comment"/>
-                  <input type="submit" hidden />
-              </form>
-            </div>
+      {this.props.postComments.map(this.renderComments)}
+      <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit.bind(this)}>
+        <input type="text" ref="author" placeholder="author"/>
+        <input type="text" ref="comment" placeholder="comment"/>
+        <input type="submit" hidden/>
+      </form>
+    </div>
   }
 }
 
--- reduxtagram/src/reducers/comments.js
+++ reduxtagram/src/reducers/comments.js
@@ -3,8 +3,37 @@
 // 2. copy of current state
 // 3 and returns the next state
 
+//handles update individual comment state
+function postComments(state = [], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      // return new state with the new comment
+      return [
+        ...state, {
+          user: action.author,
+          text: action.comment
+        }
+      ];
+    case 'REMOVE_COMMENT':
+      return [
+        ...state.slice(0, action.i),
+        ...state.slice(action.i + 1)
+      ]
+    default:
+      return state;
+  }
+  return state;
+}
+
 function comments(state = [], action) {
-  //TODO
+  if (typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // overwrite this post with a new one[action.postId]: postComments(state[action.postId], action)
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
